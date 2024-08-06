c4cc1ffafe260392f13405bc6c5305184f2dc270
CLMGurus <dev@creativelearningmachine.com>
2024-08-06 10:12:09 +0530

    testing

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -3,14 +3,10 @@ import sqlite3 from "sqlite3";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-const config = useRuntimeConfig();
-const baseURL = config.public.baseURL;
-
-console.log("Base URL:", baseURL);
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const dbPath = new URL(`./mukilam.db`, `file://${__dirname}/`).pathname;
+const dbPath = new URL(`../../server/mukilam.db`, `file://${__dirname}/`)
+  .pathname;
 
 console.log("DB patth", dbPath);
 
