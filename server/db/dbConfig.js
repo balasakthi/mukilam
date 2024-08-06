b09f87cb46282151fc2ca0aeefc286be1bd10654
CLMGurus <dev@creativelearningmachine.com>
2024-08-06 10:21:24 +0530

    testing

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -5,6 +5,9 @@ import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+console.log("file name, dirname", __filename, __dirname);
+
 const dbPath = new URL(`../../server/mukilam.db`, `file://${__dirname}/`)
   .pathname;
 
